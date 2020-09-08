19447163dff869a9e40d53afc7adcad939df332c
Arthur <arthur.albuquerque.1995@gmail.com>
2020-09-08 10:30:46 -0300

    + implementacao parcial metodo compact()

--- Graph.py
+++ Graph.py
@@ -36,9 +36,24 @@ class Graph:
             if label in self.vertex_set[v]:
                 self.vertex_set[v].remove(label)
         
-    def compact(self):
-        # TO DO
-        pass
+   """  def compact(self):
+        for i in self.vertex_set.keys():
+            save = i
+
+            while i not in self.vertex_set.keys():
+                i += 1
+            print('expressao: ' + str(len(self.vertex_set) - i))
+            
+            for j in range(len(self.vertex_set) - i):
+                newId = save + j
+                oldId = i + j
+                print('j: ', j)
+                print('newId: ' + str(newId))
+                print('oldId: ' + str(oldId))
+                self.vertex_set[newId] = self.vertex_set.pop(oldId)
+            i = save
+        return self """
+
 
     def max_degree(self):
         max_deg = 0
--- Main.py
+++ Main.py
@@ -10,19 +10,22 @@ graph.add_vertex(5)
 graph.add_vertex(6)
 graph.add_vertex(7)
 
-graph.add_edge(1,2)
-graph.add_edge(1,7)
-graph.add_edge(1,3)
-graph.add_edge(1,5)
-graph.add_edge(1,4)
-graph.add_edge(1,6)
+graph.add_arc(1,2)
+graph.add_arc(1,7)
+graph.add_arc(1,3)
+graph.add_arc(1,5)
 
-""" graph.add_arc(2,5)
+graph.add_arc(2,5)
 graph.add_arc(4,7)
 graph.add_arc(6,5)
 graph.add_arc(4,6)
 graph.add_arc(7,5)
-graph.add_arc(3,4) """
-print(str(graph))
+graph.add_arc(3,4)
 
+graph.del_vertex(5)
+graph.del_vertex(4)
+
+
+
+print(str(graph))
 print(graph.is_connected())
